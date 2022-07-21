0b5276cc62b50a7fcdcedd9c45cd1f8644f690bd
hritik01478 <hritikkumar01478@gmail.com>
2022-07-22 00:54:00 +0530

    watch page made

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Home from "./home/Home";
+import Home from "./pages/home/Home";
 import "./app.scss";
+import Watch from "./pages/watch/Watch";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Watch />
     </div>
   );
 }
