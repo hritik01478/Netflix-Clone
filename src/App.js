c1366162a6b7c008afbb8f2cd246d7c18637e9d9
hritik01478 <hritikkumar01478@gmail.com>
2022-07-22 14:50:19 +0530

    register and login page made

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Home from "./pages/home/Home";
 import "./app.scss";
 import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
+
 
 function App() {
   return (
     <div className="App">
-      <Watch />
+      <Login />
     </div>
   );
 }
